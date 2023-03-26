6b12e99bb4fbe4aaa1505d1d9fd8a66abacfd3bd
zengbai10 <2970299669@qq.com>
2023-03-26 17:16:06 +0800

    1

---
+++ 1/data.source.js
@@ -0,0 +1,112 @@
+import React from 'react';
+export const Banner00DataSource = {
+  wrapper: { className: 'banner0' },
+  textWrapper: { className: 'banner0-text-wrapper lfp5wp4yfnu-editor_css' },
+  title: {
+    className: 'banner0-title lfp58cznhet-editor_css',
+    children: (
+      <span>
+        <span>
+          <p>&nbsp;文件网盘分享beta&nbsp;&nbsp;</p>
+        </span>
+      </span>
+    ),
+  },
+  content: {
+    className: 'banner0-content',
+    children: (
+      <span>
+        <p>TT:q3zb</p>
+      </span>
+    ),
+  },
+  button: {
+    className: 'banner0-button',
+    children: (
+      <span>
+        <p>北仑大道西</p>
+      </span>
+    ),
+    href: 'https://www.123pan.com/s/qxo9-oME7d.html',
+    target: '_blank',
+  },
+};
+export const Pricing00DataSource = {
+  wrapper: { className: 'home-page-wrapper pricing0-wrapper' },
+  OverPack: { playScale: 0.3, className: 'home-page pricing0' },
+  imgWrapper: { className: 'pricing0-img-wrapper', md: 12, xs: 24 },
+  img: {
+    className: 'pricing0-img lfp68gqiaa7-editor_css',
+    name: 'image',
+    children:
+      'https://gw.alipayobjects.com/mdn/rms_ae7ad9/afts/img/A*OnyWT4Nsxy0AAAAAAAAAAABjARQnAQ',
+  },
+  childWrapper: {
+    className: 'pricing0-text-wrapper',
+    md: 12,
+    xs: 24,
+    children: [
+      {
+        name: 'title',
+        children: (
+          <span>
+            <span>
+              <span>
+                <h2>
+                  <i>
+                    <b>(赞助商)</b>
+                  </i>&nbsp;rainbow cat
+                </h2>
+              </span>
+            </span>
+          </span>
+        ),
+        className: 'pricing0-title',
+      },
+      {
+        name: 'content',
+        children: (
+          <span>
+            <p>绕过网易的检测，有最新的包</p>
+          </span>
+        ),
+        className: 'pricing0-content',
+      },
+      {
+        name: 'pricing',
+        children: (
+          <span>
+            <p>¥10,000,000</p>
+          </span>
+        ),
+        className: 'pricing0-pricing',
+      },
+      {
+        name: 'button',
+        children: {
+          icon: 'shopping-cart',
+          href: '#',
+          type: 'primary',
+          children: '立即购买',
+        },
+      },
+    ],
+  },
+};
+export const Footer00DataSource = {
+  wrapper: { className: 'home-page-wrapper footer0-wrapper' },
+  OverPack: { className: 'home-page footer0', playScale: 0.05 },
+  copyright: {
+    className: 'copyright',
+    children: (
+      <span>
+        <span>
+          <span>
+            <span>©2018</span>
+          </span>
+        </span>
+        <span>&nbsp;由zengbai运营 没有备案号</span>
+      </span>
+    ),
+  },
+};
